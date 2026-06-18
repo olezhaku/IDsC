eab0d2ea5b67649d5ba8b134a9cf968f7728eb0b
olezhaku <139953855+olezhaku@users.noreply.github.com>
2026-06-18 23:30:32 +0900

    decomposition

--- app/src/main/java/com/olezhaku/idsc/MainActivity.kt
+++ app/src/main/java/com/olezhaku/idsc/MainActivity.kt
@@ -3,38 +3,29 @@ package com.olezhaku.idsc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.input.TextFieldLineLimits
-import androidx.compose.foundation.text.input.rememberTextFieldState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
+import com.olezhaku.idsc.ui.components.Input
+import com.olezhaku.idsc.ui.components.InputType
 import com.olezhaku.idsc.ui.theme.IDsCTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
-            IDsCTheme() {
+            IDsCTheme {
                 MainScreen()
             }
         }
@@ -44,49 +35,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val focusManager = LocalFocusManager.current
+    var searchText by remember { mutableStateOf("") }
 
-    Scaffold(
-        containerColor = MaterialTheme.colorScheme.background
-    ) { innerPadding ->
+    Scaffold { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
-                .clickable(
-                    indication = null,
-                    interactionSource = remember { MutableInteractionSource() }
-                ) {
-                    focusManager.clearFocus()
-                }
                 .padding(24.dp)
         ) {
-            Input()
-        }
-    }
-}
-
-@Composable
-fun Input() {
-    var isFocused by remember { mutableStateOf(false) }
-
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth().onFocusChanged { focusState ->
-            isFocused = focusState.isFocused
-        },
-
-        shape = androidx.compose.foundation.shape.RoundedCornerShape(12.dp),
-        state = rememberTextFieldState(),
-        lineLimits = TextFieldLineLimits.SingleLine,
-        trailingIcon = {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = "Поиск",
-                tint = if (isFocused) {
-                    MaterialTheme.colorScheme.primary
-                } else {
-                    MaterialTheme.colorScheme.onSurfaceVariant
-                }
+            Input(
+                value = searchText,
+                onValueChange = { searchText = it },
+                clearFocus = { focusManager.clearFocus() },
+                type = InputType.Search
             )
         }
-    )
+    }
 }
